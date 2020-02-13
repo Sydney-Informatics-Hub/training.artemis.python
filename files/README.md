389919e9c595f6f9dee2790418052f962431eec5
Kristian Maras <kristian.maras@sydney.edu.au>
2020-02-13 11:41:21 +1100

    adding extra intro

---
+++ files/README.md
@@ -0,0 +1,13 @@
+# hpc
+training material and tools for HPC use
+Under development and the moment.
+
+Main goal: develop working examples that assist hpc use with python,
+using modern libraries and techniques
+
+1. Using simple functions (calculation of pi) in parallel manner using python multiprocessing
+2. Intermediate scaling (mpi4py, threads, numba 
+3. Working with large data and functions
+4. Simulation and Visualisation cases (pangeo, dask etc)
+
+
---
+++ files/basic_parallel/computepi_pawsey.py
@@ -0,0 +1,35 @@
+#https://github.com/PawseySC/Intermediate-Supercomputing/blob/master/darts/serial/python/darts.py
+#adapted from pawsey
+
+import numpy.random as rng
+import time
+
+def pi_run(num_trials):
+    """Calculate pi based on points with a box and circle of radius 1
+    """
+    r = 1.0
+    r2 = r * r
+    Ncirc = 0
+    for _ in range(num_trials):
+        x = rng.random() 
+        y = rng.random()
+        if ( x**2 + y**2 < r2 ):
+            Ncirc +=1 
+    pi = 4.0 * ( float(Ncirc) / float(num_trials) ) 
+    return pi
+
+def cycle_pi():
+    """Cycle through a list of trials of increasing magnitude
+    """
+    num_trials = [10**1,10**2,10**3,10**4,10**5,10**6,10**7]
+    pi_estimates = []
+    for num_trial in num_trials:
+        pi = pi_run(num_trial)
+        pi_estimates.append(pi)
+    return pi_estimates
+
+if __name__ == "__main__":
+    start_time = time.time()
+    pi = cycle_pi()
+    duration = time.time() - start_time
+    print(f"Pi convergence : {pi} calculated in {duration} seconds")
