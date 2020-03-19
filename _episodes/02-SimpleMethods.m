20e3e88b21087e79bc28d0f83c93172668145f84
natbutter <natbutter@gmail.com>
2020-03-19 15:40:51 +1100

    Create 02-SimpleMethods.m

---
+++ _episodes/02-SimpleMethods.m
@@ -0,0 +1,35 @@
+---
+title: "Simple methods"
+teaching: 15
+exercises: 0
+questions:
+- "Which method for accelration should I choose?"
+objectives:
+- "Learn about the speed differences in Loops, Iterators, and Generators"
+- "See how numpy and pandas use Vectorising to improve perfomance for some data"
+- "Multithreading"
+- "Use MPI to communicate betwwen workers"
+keypoints:
+- "Understand there are different ways to accelerate"
+- "The best method depends on your algorithms, code and data"
+---
+This episode shows you a few of the basic tools that we can use in Python to make our code go faster.
+
+# Loops, Iterators, and Genorators
+
+# Vectorising code with numpy and pandas
+
+# Multi-threading/processing 
+
+# MPI: Message Passing Interface
+MPI is a standardized and portable message-passing system designed to function on a wide variety of parallel computers.
+The standard defines the syntax and semantics of a core of library routines useful to a wide range of users writing portable message-passing programs in C, C++, and Fortran. There are several well-tested and efficient implementations of MPI, many of which are open-source or in the public domain.
+
+http://openmpi.org
+
+http://mpich.org
+
+**MPI for Python**
+mpi4py provides bindings of the MPI standard for the Python programming language, allowing any Python program to exploit multiple processors.
+
+
